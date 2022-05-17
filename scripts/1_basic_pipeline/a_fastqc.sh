8756db2517581e9449727e35f44f23e428081f80
Jack Gisby <jackgisby@gmail.com>
2022-05-17 17:00:03 +0100

    Add basic QC scripts

---
+++ scripts/1_basic_pipeline/a_fastqc.sh
@@ -0,0 +1,16 @@
+
+# the variable $1 is used to get the argument we passed to this script
+echo $1
+
+# we get the value of the row specified by $1 here
+fastq_srr="$(head -"$1" data/files.txt | tail -1)"
+echo $fastq_srr
+
+# so, this is the location of the .fastq file we need
+fastq_location="data/$fastq_srr.fastq"
+echo $fastq_location
+
+mkdir results/1_basic_pipeline/a_fastqc
+
+# run fastqc on the specified fastq file
+fastqc $fastq_location -o results/1_basic_pipeline/a_fastqc
