e27b6df90a96c299068488186dd4b39d7388b555
Jack Gisby <jackgisby@gmail.com>
2022-05-18 23:11:30 +0100

    Simplify fastqc script to take fastq as input

--- scripts/1_basic_pipeline/a_fastqc.sh
+++ scripts/1_basic_pipeline/a_fastqc.sh
@@ -1,14 +1,6 @@
 
-# the variable $1 is used to get the argument we passed to this script
-echo $1
-
-# we get the value of the row specified by $1 here
-fastq_srr="$(head -"$1" data/files.txt | tail -1)"
-echo $fastq_srr
-
-# so, this is the location of the .fastq file we need
-fastq_location="data/$fastq_srr.fastq.gz"
-echo $fastq_location
+# $1 = the results directory (e.g. results/1_basic_pipeline/a_fastqc)
+# $2 = the path of the fastq file (e.g. data/fastq/SRR391535.fastq.gz)
 
 # run fastqc on the specified fastq file
-fastqc $fastq_location -o results/1_basic_pipeline/a_fastqc
+fastqc -o $1 $2
