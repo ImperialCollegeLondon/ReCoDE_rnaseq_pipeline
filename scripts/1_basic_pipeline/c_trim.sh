873e61ceee642a08e185ea8d4bf6ab8e7151fbc7
Jack Gisby <jackgisby@gmail.com>
2022-05-18 23:12:38 +0100

    Add trimming script

---
+++ scripts/1_basic_pipeline/c_trim.sh
@@ -0,0 +1,6 @@
+
+# $1 = the results directory (e.g. results/1_basic_pipeline/c_trim)
+# $2 = the path of the fastq file (e.g. data/fastq/SRR391535.fastq.gz)
+
+# run fastqc on the specified fastq file
+trim_galore --fastqc -o $1 $2
