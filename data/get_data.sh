f832c3b2f4df34223b9f85cf2a2e7354ec4d11f8
Jack Gisby <jackgisby@gmail.com>
2022-05-18 23:10:25 +0100

    Download data to specific directories

--- data/get_data.sh
+++ data/get_data.sh
@@ -3,11 +3,12 @@
 # module load sratoolkit/2.8.1
 # export PATH=$PATH:~/apps/sratoolkit.3.0.0-ubuntu64/bin
 
-# a vector of the SRR identifiers for the RNA-seq samples
+# a vector of the sequence read archive identifiers for the RNA-seq samples
 #            1           2           3           4           5           6
 SAMPLE_SRR=("SRR391535" "SRR391536" "SRR391537" "SRR391538" "SRR391539" "SRR391541")
 
 # create a file to list the IDs and file locations
+rm -f data/files.txt
 touch data/files.txt
 
 # for each sample
@@ -19,7 +20,18 @@ for srr in ${SAMPLE_SRR[@]}; do
     # create file listing the ID and file location
     echo $srr >> data/files.txt
 
-    # get the sample's file
-    fastq-dump --gzip "$srr"
+    # get the sample's file from the sequence read archive
+    fastq-dump --gzip -O data/fastq/ "$srr"
 
 done
+
+# get the genome and annotation files from NCBI
+# https://www.ncbi.nlm.nih.gov/assembly/GCF_000004515.6
+
+mkdir data/genome
+
+wget -O data/genome/GCF_000004515.6_Glycine_max_v4.0_genomic.fna.gz \
+     "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/004/515/GCF_000004515.6_Glycine_max_v4.0/GCF_000004515.6_Glycine_max_v4.0_genomic.fna.gz"
+
+wget -O data/genome/GCF_000004515.6_Glycine_max_v4.0_genomic.gtf.gz \
+     "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/004/515/GCF_000004515.6_Glycine_max_v4.0/GCF_000004515.6_Glycine_max_v4.0_genomic.gtf.gz"
