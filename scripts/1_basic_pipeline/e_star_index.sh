cb721609c00f1dde1e89874699d50591f0ff3192
Jack Gisby <jackgisby@gmail.com>
2022-05-19 17:17:30 +0100

    Unzip files before running STAR

--- scripts/1_basic_pipeline/e_star_index.sh
+++ scripts/1_basic_pipeline/e_star_index.sh
@@ -2,10 +2,22 @@
 # $1 = directory to save indices (e.g. results/1_basic_pipeline/e_star_index/)
 # $2 = genome fasta files (e.g. results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.fna)
 # $3 = genome annotation file file (e.g. results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.gtf)
+# $4 = sparsity (can be set to non-zero values to reduce the memory STAR needs)
+# $5 = number of threads for STAR to use
+
+# genome files must be unzipped for STAR indexing
+gzip -cfdk $2 > results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.fna
+gzip -cfdk $3 > results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.gtf
 
 # generate genome indices
-# can use --genomeSAsparseD 2 as an argument if STAR uses too much memory
+# can use --genomeSAsparseD as an argument if STAR uses too much memory
 STAR --runMode genomeGenerate \
      --genomeDir $1 \
-     --genomeFastaFiles $2 \
-     --sjdbGTFfile $3
+     --genomeFastaFiles results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.fna \
+     --sjdbGTFfile results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.gtf \
+     --genomeSAindexNbases 13 \
+     --genomeSAsparseD $4 \
+     --runThreadN $5
+
+# remove unzipped fasta
+rm results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.fna
