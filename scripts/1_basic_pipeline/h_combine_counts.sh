36e10161eb04bdb5dfab37e70172e6b99b432833
Jack Gisby <jackgisby@gmail.com>
2022-05-19 17:17:45 +0100

    Remove count merge stage

--- scripts/1_basic_pipeline/h_combine_counts.sh
+++
@@ -1,5 +0,0 @@
-
-# $1 = directory containing counts (e.g. results/1_basic_pipeline/g_htseq_count/)
-# $2 = output file path (e.g. results/1_basic_pipeline/h_combine_counts/combined_counts.txt)
-
-$1
