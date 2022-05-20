25246ce94b6ed2925f3fb84f59f8895fd9676133
Jack Gisby <jackgisby@gmail.com>
2022-05-20 16:48:34 +0100

    Remove cores argument to trimgalore

--- bin/c_trim.sh
+++ bin/c_trim.sh
@@ -1,7 +1,7 @@
 
 # $1 = the results directory (e.g. results/1_basic_pipeline/c_trim)
 # $2 = the path of the fastq file (e.g. data/fastq/SRR391535.fastq.gz)
-# $3 = the number of cores to use
 
 # run fastqc on the specified fastq file
-trim_galore --fastqc -o $1 --cores $3 $2
+trim_galore --fastqc -o $1 $2
+# --fastqc_args "<ARGS>"
