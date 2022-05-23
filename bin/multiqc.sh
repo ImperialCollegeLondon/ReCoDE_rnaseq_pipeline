35420f0ec5a6523cb2c2ac5ded67a8df167ba52a
Jack Gisby <jackgisby@gmail.com>
2022-05-23 09:52:36 +0100

    Combine multiqc scripts

---
+++ bin/multiqc.sh
@@ -0,0 +1,7 @@
+
+# $1 = the results directory (e.g. results/1_basic_pipeline/b_multiqc)
+# $2 = directory 1 (e.g. results/1_basic_pipeline/a_fastqc)
+# $3 = directory 2 (e.g. results/1_basic_pipeline/f_align_and_count)
+
+# group together fastqc reports
+multiqc -o $1 $2 $3
