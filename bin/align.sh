84a307124e5bfdc08f0dad35edfd5818a17af167
Jack Gisby <jackgisby@gmail.com>
2022-05-31 11:20:01 +0100

    Split align and count into separate scripts

--- bin/align.sh
+++ bin/align.sh
@@ -3,7 +3,6 @@
 # $2 = the location of the trimmed fastq file (e.g. results/1_basic_pipeline/c_trim/SRR391535_trimmed.fq.gz)
 # $3 = the prefix of the output file (e.g. results/1_basic_pipeline/f_star_align/SRR391535)
 # $4 = the number of threads for STAR to use
-# $5 = the annotation file (e.g. results/1_basic_pipeline/e_star_index/GCF_000004515.6_Glycine_max_v4.0_genomic.gtf) 
 
 # unzip fastq for alignment
 gzip -cfdk $2 > $3.fastq
@@ -17,6 +16,3 @@ STAR --genomeDir $1 \
 
 # index the alignment for htseq
 samtools index $3Aligned.sortedByCoord.out.bam
-
-# use alignment to get counts
-htseq-count -s no -r pos -f bam $3Aligned.sortedByCoord.out.bam $5 > $3.counts
